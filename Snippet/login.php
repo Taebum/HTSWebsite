4856bc9cf77172873695019c049b764e24b3855f
Taebum <TS10005@D10005.skolan.hassleholm.se>
2013-03-18 10:55:10 +0100

    kommentar

--- Snippet/login.php
+++ Snippet/login.php
@@ -1,12 +1,14 @@
 ﻿<!-- Kom ihåg att logga in till databasen först! -->
 <?php
+		include '../Demo/connect.php';
+		
          $query = "SELECT name FROM user WHERE name='$_POST[myUser]' AND password='$_POST[myPassword]'";
-         //echo '<em> ' . $query . ' </em>';
+         echo '<em> ' . $query . ' </em>';
          $result = mysql_query($query);
          if (mysql_numrows($result) == 1) {
 		      session_start();
 		      $_SESSION['session_user']=$_POST[myUser];
-			  header('Location: index.php');
+			  header('Location: index.html');
 		 }
 		 else {
 			  header('Location: login.html');
